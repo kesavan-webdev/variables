9dad944b1bfb02ef12c3223b680c56a6d4acaea8
kesavan <kesavanplk7@gmail.com>
2023-02-15 02:09:02 -0500

    variables

--- script/script.js
+++ script/script.js
@@ -1,24 +1,37 @@
-//block scope 
-const country="india";
+//JavaScript (JS) is a lightweight, interpreted, or just-in-time dynamic compiled programming language
+//Ecmascript is official Name
+
+//VARIABLES
+
+//block scope
+const country = "india";
 //const country="usa"  //it will throw an error we cant redeclare
-//country="uk"          //it will throw an error we cant reinitiallize
-const continent="asia";    
+//country="uk"  //it will throw an error we cant reinitiallize
+//const country;        //it will throw an error we cant reinitiallize
+const continent = "asia";
 
-
-let population=140.76;
+let population = 140.76;
 // let population = 140; it will throw an error we cant redeclare
-       population=140; //it will take because we reinitiallize
+population = 140; //it will take because we reinitiallize
+
+let connected; //it will work
 
 //global scope
-var userName="kesav";  
-var userName = "kumar";//it doesnt throw error
-userName = "dinesh";   //it doesnt throw error
-  
+var userName = "kesav";
+var userName = "kumar"; //it doesnt throw error
+userName = "dinesh"; //it doesnt throw error
 
 //  =  assignment operator
 
 // output
 console.log(country);
-console.log(continent);   
+console.log(continent);
 console.log(population);
-console.log(userName)
+console.log(userName);
+
+//DATA-TYPES
+
+//we dont need to specify int,string,boolean,float etc..
+
+const isIsland = false;
+let language;
